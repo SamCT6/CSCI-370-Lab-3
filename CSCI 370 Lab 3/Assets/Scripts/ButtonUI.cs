1b4960e9585039b97cc164240505a3c749a8b2c7
JoryFulme1917 <fulmerjory@gmail.com>
2025-04-28 20:58:57 -0500

    Added level transitions between screens

--- CSCI 370 Lab 3/Assets/Scripts/ButtonUI.cs
+++ CSCI 370 Lab 3/Assets/Scripts/ButtonUI.cs
@@ -1,10 +1,20 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 public class ButtonUI : MonoBehaviour
 {
+    [SerializeField] Animator transition;
     [SerializeField] private string newGameLevel = "Instruction Scene";
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void NewGameButton(){
+        StartCoroutine(Loadlevel());
+    }
+
+    IEnumerator Loadlevel()
+    {
+        transition.SetTrigger("Fadeout");
+        yield return new WaitForSeconds(1);
         SceneManager.LoadScene(newGameLevel);
+        transition.SetTrigger("Fadein");
     }
 }
